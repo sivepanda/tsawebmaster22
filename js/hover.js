2d12b469123033fefa3c9410850440f2da1a2ce1
sivepanda <68242448+sivepanda@users.noreply.github.com>
2021-12-28 16:49:06 -0600

    polish desktop website

--- js/hover.js
+++ js/hover.js
@@ -1,5 +1,6 @@
 var right = document.getElementsByClassName("biglinkright");
 var left = document.getElementsByClassName("biglinkleft");
+var h2 = document.getElementsByClassName("biglink");
 
 window.onload = function() {
     left[0].style.opacity = "1";
@@ -8,15 +9,21 @@ window.onload = function() {
 
 right[0].onmouseover = function() {
     left[0].style.opacity = "0";
-    right.style.color = "rgb(25, 71, 2)";
+    h2[1].style.fontSize = "9vh";
+    h2[0].style.fontSize = "12vh";
 };
 left[0].onmouseover = function() {
     right[0].style.opacity = "0";
+    h2[0].style.fontSize = "9vh";
+    h2[1].style.fontSize = "12vh";
 };
 right[0].onmouseout = function() {
+    h2[1].style.fontSize = "10vh";
+    h2[0].style.fontSize = "10vh";
     left[0].style.opacity = "1";
-    console.log("a");
 };
 left[0].onmouseout = function() {
+    h2[0].style.fontSize = "10vh";
+    h2[1].style.fontSize = "10vh";
     right[0].style.opacity = "1";
 };
--- js/listinfo.js
+++ js/listinfo.js
@@ -2,45 +2,57 @@ var titlebox = document.getElementById("ec2");
 var titles = document.getElementsByClassName("title");
 var descriptions = document.getElementsByClassName("description");
 
-titles[0].onmouseover = function() {
-    titlebox.style.background = "url('resources/images/bass_dim.png')";
-    titlebox.style.backgroundPosition = "top center";
-    titlebox.style.backgroundSize = "cover";
+window.onload = function() {
     descriptions[0].style.opacity = "1";
 };
-titles[1].onmouseover = function() {
-    titlebox.style.background = "url('resources/images/droole_dim.png')";
+
+titlebox.onmouseout = function() {
+    descriptions[0].style.opacity = "1";
+}
+
+titles[0].onmouseover = function() {
+    descriptions[0].style.opacity = "0";
+    titlebox.style.background = "url('resources/images/bass_dim.png')";
     titlebox.style.backgroundPosition = "top center";
     titlebox.style.backgroundSize = "cover";
     descriptions[1].style.opacity = "1";
 };
-titles[2].onmouseover = function() {
-    titlebox.style.background = "url('resources/images/smore_dim.png')";
+titles[1].onmouseover = function() {
+    descriptions[0].style.opacity = "0";
+    titlebox.style.background = "url('resources/images/droole_dim.png')";
     titlebox.style.backgroundPosition = "top center";
     titlebox.style.backgroundSize = "cover";
     descriptions[2].style.opacity = "1";
 };
-titles[3].onmouseover = function() {
-    titlebox.style.background = "url('resources/images/pd_dim.png')";
+titles[2].onmouseover = function() {
+    descriptions[0].style.opacity = "0";
+    titlebox.style.background = "url('resources/images/smore_dim.png')";
     titlebox.style.backgroundPosition = "top center";
     titlebox.style.backgroundSize = "cover";
     descriptions[3].style.opacity = "1";
 };
+titles[3].onmouseover = function() {
+    descriptions[0].style.opacity = "0";
+    titlebox.style.background = "url('resources/images/pd_dim.png')";
+    titlebox.style.backgroundPosition = "top center";
+    titlebox.style.backgroundSize = "cover";
+    descriptions[4].style.opacity = "1";
+};
 //mouseout
 titles[0].onmouseout = function() {
     titlebox.style.background = "";
-    descriptions[0].style.opacity = "0";
+    descriptions[1].style.opacity = "0";
 };
 titles[1].onmouseout = function() {
     titlebox.style.background = "";
-    descriptions[1].style.opacity = "0";
+    descriptions[2].style.opacity = "0";
 };
 titles[2].onmouseout = function() {
-    descriptions[2].style.opacity = "0";
+    descriptions[3].style.opacity = "0";
     titlebox.style.background = "";
 
 };
 titles[3].onmouseout = function() {
     titlebox.style.background = "";
-    descriptions[3].style.opacity = "0";
+    descriptions[4].style.opacity = "0";
 };
